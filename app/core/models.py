48500eeb9257b51fc3df963f58256d4b1d01fb4e
Devw4lL <dev_w4lL@protonmail.com>
2022-09-16 17:14:11 +0200

    Add user API view user .

--- app/core/models.py
+++ app/core/models.py
@@ -1,27 +1,38 @@
 """
-Tests Database.
+Database models.
 """
 
 from django.db import models
 from django.contrib.auth.models import (
-    AbstractUser,
+    AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
 )
 
 
 class UserManager(BaseUserManager):
-    """Manage for users."""
-    def create_user(self, email, password=None, **extra_field):
+    """Manager for users."""
+    def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_field)
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
+    def create_superuser(self, email, password):
+        """Create and return superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
-class User(AbstractUser, PermissionsMixin):
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
