b009e2df638ab9ec6fa599e0ef23376d79d35c08
Devw4lL <dev_w4lL@protonmail.com>
2022-09-20 11:46:31 +0200

    Auth token for API side.

--- app/user/views.py
+++ app/user/views.py
@@ -8,7 +8,7 @@ from rest_framework.settings import api_settings
 
 from user.serializers import (
     UserSerializer,
-    AuthTokenSerializer
+    AuthTokenSerializer,
 )
 
 
@@ -23,11 +23,11 @@ class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-class ManageUserView(generics.RetrieveAPIView):
+class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
-    permissions_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         """Retrieve and return the authenticated user."""
